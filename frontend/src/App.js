f25f9fbc1d5b5b2a411d98b6e7ac7f9566f10ac2
j-b-rams <josh.abrams111@gmail.com>
2023-03-19 01:23:33 -0400

    importing register js to app js
    Forgot to add this to the last push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
