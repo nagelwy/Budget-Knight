89bf5a187a813f1ad86b93a2ed5b0f443d9d567c
Megan Carlson <102761350+meglc@users.noreply.github.com>
2023-04-08 17:43:50 -0400

    updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -18,11 +18,25 @@ function Home () {
         <React.Fragment>
            <NavBar/>
         </React.Fragment>
-        <h1 className="welcome">Welcome Knights, <br /> Let's take your first step towards <br /> Financial Freedom </h1>
-        <div className = "idk">
-        <button className = "home-signup-btn" onClick={goToSignUp}> 
-            Get Started
-            </button>
+        <div>
+            <section className = "home" id = "home"> 
+            <h1 className="welcome">Welcome Knights, <br /> Let's take your first step towards <br /> Financial Freedom </h1>
+            <div className = "idk">
+            <button className = "home-signup-btn" onClick={goToSignUp}> 
+                Get Started
+                </button>
+            </div>
+            </section>
+            <section className = "mission" id = "mission"> 
+            <h1>Mission Statement</h1>
+            <p>Our goal is to help our fellow knights develop better spending habits 
+                and take that first step towards financial freedom. We understand 
+                everyone has their own financial needs, which is why we aimed to 
+                make Budget Knight customizable to fit any and all lifestyles. </p>
+            </section>
+            <section className = "services" id = "services"> Services </section>
+            <section className = "contact" id = "contact"> Contact Us </section>
+            <section className = "rest"></section>
         </div>
     </div>
     )
